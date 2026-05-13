d2c119c1cf3f0cc5d2e83c88bcdd4dd2bfb39c76
sambntz <sbenitez.js@gmail.com>
2026-05-12 21:45:39 -0300

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const mysql = require('mysql2/promise')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const app = express()
+
+const config = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10
+}
+
+console.log({ config });
+
+const pool = mysql.createPool(config)
+
+app.get('/products', async (req, res) => {
+    try {
+        const [rows] = await pool.query(
+            'SELECT id, name FROM products'
+        )
+
+        res.json(rows)
+    } catch (error) {
+        console.error(error)
+
+        res.status(500).json({
+            error: 'Internal server error'
+        })
+    }
+})
+
+app.listen(process.env.APP_PORT, () => {
+    console.log(`Server running on http://localhost:${process.env.APP_PORT}`)
+})
